6e0ce3c737b70c23b5db1711d20f162d3acec4b8
Jacky Liao <jackyliao2008@gmail.com>
2019-02-02 22:14:22 +0000

    More serial number handling

--- tests/test.cpp
+++ tests/test.cpp
@@ -14,6 +14,8 @@
 
 using namespace wlp;
 
+uint32_t serial;
+
 int get_serial() {
 	int fd = open("/dev/vcio", 0);
 	if (fd == -1)
@@ -49,9 +51,29 @@ int get_serial() {
 	return property[5];
 }
 
+void read_thread(canbus *bus) {
+    uint32_t id;
+    uint8_t data[8];
+    uint8_t len;
+    bool req;
+
+    while(true) {
+        if(!bus->recv(&id, data, &len, &req)) {
+            return;
+        }
+		if(id == 64) {
+			uint32_t remote_serial = *(uint32_t *) data;
+			printf("Remote serial received: %x\n", remote_serial);
+			if(remote_serial < serial) {
+				printf("Setting as slave\n");
+			}
+		}
+    }
+}
+
 int main() {
 
-	uint32_t serial = get_serial();
+	serial = get_serial();
 
 	printf("%x\n", serial);
 
@@ -60,33 +82,14 @@ int main() {
         return 1;
     }
 
+	std::thread thread(read_thread, &bus);
+
 	while(true) {
 		if(!bus.send(64, (uint8_t *) &serial, 4)) {
 			return 1;
 		}
 		usleep(10000);
 	}
-	
-
-    uint32_t id;
-    uint8_t data[8];
-    uint8_t len;
-    bool req;
-
-    while(true) {
-        if(!bus.recv(&id, data, &len, &req)) {
-            return 1;
-        }
-        printf("Recv: %d (%d) ", id, len);
-        if(!req) {
-            for(uint8_t i = 0; i < len; ++i) {
-                printf("%02x ", data[i]);
-            }
-            printf("\n");
-        } else {
-            printf("remote request\n");
-        }
-    }
 
     return 0;
 }
